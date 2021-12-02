7f33874412470c8ed9f7a942b8b0caeac146f682
Abdullah_assoli <90969574+ASSOLI99@users.noreply.github.com>
2021-12-02 05:10:04 +0200

    Update and rename Untitled-3.js to Untitled.js

--- Untitled.js
+++ Untitled.js
@@ -2,13 +2,16 @@ let a= 1_00;
 let b = 2_00.5;
 let c = 1e2;
 let d = 2.4;
+//smallest nuber frome variables
 console.log(Math.trunc(Math.min(a,b,c,d)));
+//in a+b with no repeat =10000
 console.log(Math.pow(d,0)*Math.pow(a,2));
+//make d int in 4 methods
 console.log(Math.round(d));
 console.log(Math.floor(d));
 console.log(Math.ceil(d));
 console.log(Math.trunc(d));
+//in b+d with no repeat 1:=66.67  2:=67
 console.log(parseFloat(Math.floor(b)/Math.ceil(d)).toFixed(2));
 console.log(Math.ceil(Math.floor(b)/Math.ceil(d)));
-
 
