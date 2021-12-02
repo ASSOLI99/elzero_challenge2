b39c04e5c082e60693d879ec7fcd061951e143af
Abdullah_assoli <90969574+ASSOLI99@users.noreply.github.com>
2021-12-02 05:10:30 +0200

    Update Untitled.js

--- Untitled.js
+++ Untitled.js
@@ -2,7 +2,7 @@ let a= 1_00;
 let b = 2_00.5;
 let c = 1e2;
 let d = 2.4;
-//smallest nuber frome variables
+//smallest nuber from variables
 console.log(Math.trunc(Math.min(a,b,c,d)));
 //in a+b with no repeat =10000
 console.log(Math.pow(d,0)*Math.pow(a,2));
